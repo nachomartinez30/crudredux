c85a6a8dd6a1cd544a10ede39145d0a966a7c865
nachomartinez30 <nachomartinez3010@gmail.com>
2020-07-28 14:08:59 -0500

    se agregan estilos y componentes

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <nav className='navbar navbar-expand-lg navbar-e'>
+        
+        </nav>
+    );
+}
+
+export default Header;
